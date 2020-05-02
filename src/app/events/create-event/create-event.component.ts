5495f0f03cb7b79980601447fce1c48f0d2bafe0
Savitri <savitri26m@gmail.com>
2020-05-03 00:58:34 +0530

    demo edit form using create form

--- src/app/events/create-event/create-event.component.ts
+++ src/app/events/create-event/create-event.component.ts
@@ -12,9 +12,23 @@ export class CreateEventComponent implements OnInit {
   constructor(private router: Router,
     private eventService: EventsListService) { }
 
-  newEvent: any;
+  event: any;
 
   ngOnInit(): void {
+    this.event = {
+      name: 'ng Spectacular',
+      date: '12/12/2020',
+      time: '10 am',
+      price: 445.78,
+      location: {
+        address: '345 Happy St',
+        city: 'Mumbai',
+        country: 'India'
+      },
+      onlineUrl: 'https://ngSpectacular.com',
+      imageUrl: 'https://ngSpectacular.com/logo.png'
+    }
+
   }
 
   saveEvent(formValues){
