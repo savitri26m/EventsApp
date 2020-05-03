7c26a45c35b7f2e5912e2486a4e532ff2a578912
Savitri <savitri26m@gmail.com>
2020-05-03 23:00:58 +0530

    adding multiple ng-content

--- src/app/events/collapsable-well/collapsable-well.component.ts
+++ src/app/events/collapsable-well/collapsable-well.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-collapsable-well',
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CollapsableWellComponent implements OnInit {
 
-  @Input() sessionTitle: string;
+  // @Input() sessionTitle: string;
   visible: boolean = false;
  
   constructor() { }
